f8ff60b89d1f3cb6803b0f35f4eb888ed3242ac9
Pinky Gautam <pkgtm1998@gmail.com>
2024-05-16 22:26:42 +0700

    Create staging.py

---
+++ staging.py
@@ -0,0 +1,39 @@
+from pyspark.sql import DataFrame
+from pyspark.sql.functions import col, isnotnull, isnull, lit
+from common import clean_map, get_upsert_query, stg_delete_query
+from processor import Processor
+
+
+class TableProcessor(Processor):
+
+    def upsert_records(self, df: DataFrame, batch_id: int):
+        temp_table = "staging.upsert_" + self.table_name
+        staging_table = "staging." + self.table_name
+        df = df.withColumn("is_deleted", lit(False))
+        self.__stage_stream(df, temp_table)
+        self.__engine.execute(get_upsert_query(staging_table, temp_table, df.columns))
+
+    def delete_records(self, df: DataFrame, batch_id: int):
+        temp_table = "staging.delete_" + self.table_name
+        staging_table = "staging." + self.table_name
+        df = df.withColumn("is_deleted", lit(True))
+        self.__stage_stream(df, temp_table)
+        self.__engine.execute(
+            stg_delete_query.format(stg=staging_table, temp=temp_table)
+        )
+
+    def load_stream(self):
+        (
+            self.data.filter(isnotnull(col("after")))
+            .select("after.*")
+            .selectExpr(*clean_map[self.table_name])
+            .writeStream.foreachBatch(self.upsert_records)
+            .start()
+        )
+        (
+            self.data.filter(isnotnull(col("before")) & isnull(col("after")))
+            .select("before.*")
+            .selectExpr(*clean_map[self.table_name])
+            .writeStream.foreachBatch(self.delete_records)
+            .start()
+        )
