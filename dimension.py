9df38496679e42560cd399335775985e37bc85be
Pinky Gautam <pkgtm1998@gmail.com>
2024-05-16 22:26:07 +0700

    Create dimension.py

---
+++ dimension.py
@@ -0,0 +1,54 @@
+from pyspark.sql import DataFrame
+from pyspark.sql.functions import col, isnotnull, isnull
+from common import clean_map, dim_upsert_query, dim_delete_query
+from processor import Processor
+
+
+class DimensionProcessor(Processor):
+
+    def __init__(
+        self,
+        dimension: str,
+        username: str,
+        password: str,
+        host: str,
+        database: str,
+        temp_dir: str,
+        aws_region: str,
+    ):
+        super().__init__(
+            dimension, username, password, host, database, temp_dir, aws_region
+        )
+
+    def upsert_records(self, df: DataFrame, batch_id: int):
+        staging_table = "staging.upsert_" + self.table_name
+        dimension_table = "dim_" + self.table_name
+        self.stage_stream(df, staging_table)
+        self.__engine.execute(
+            dim_upsert_query.format(dim=dimension_table, stage=staging_table)
+        )
+        self.stage_stream(df, dimension_table, "append")
+
+    def delete_records(self, df: DataFrame, batch_id: int):
+        staging_table = "staging.delete_" + self.table_name
+        dimension_table = "dim_" + self.table_name
+        self.stage_stream(df, staging_table)
+        self.__engine.execute(
+            dim_delete_query.format(dim=dimension_table, stage=staging_table)
+        )
+
+    def load_stream(self):
+        (
+            self.data.filter(isnotnull(col("after")))
+            .select("after.*")
+            .selectExpr(*clean_map[self.table_name])
+            .writeStream.foreachBatch(self.upsert_records)
+            .start()
+        )
+        (
+            self.data.filter(isnotnull(col("before")) & isnull(col("after")))
+            .select("before.*")
+            .selectExpr(*clean_map[self.table_name])
+            .writeStream.foreachBatch(self.delete_records)
+            .start()
+        )
