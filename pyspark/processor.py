7d753bf7ebb6327669add55543120fd7c7da39ad
Pinky Gautam <pkgtm1998@gmail.com>
2024-06-02 14:15:04 +0700

    Update processor.py

--- pyspark/processor.py
+++ pyspark/processor.py
@@ -5,48 +5,30 @@ import redshift_connector
 
 
 class Processor:
-    __max_offsets = 150
+    __max_messages = 100
 
-    def __init__(
-        self,
-        table_name: str,
-        username: str,
-        password: str,
-        host: str,
-        database: str,
-        temp_dir: str,
-        aws_region: str,
-    ):
+    def __init__(self, table_name: str, project_id: str, zone: str):
         self.table_name = table_name
-        self.redshift_parameters = {
-            "user": username,
-            "password": password,
-            "database": database,
-            "host": host.split(":")[0],
-            "port": host.split(":")[-1],
-        }
-        self.__jdbc_url = (
-            f"jdbc:redshift://{host}/{database}?user={username}&password={password}"
-        )
-        self.temp_dir = temp_dir
-        self.aws_region = aws_region
+        self.project_id = project_id
+        self.zone = zone
 
-    def read_stream(self, spark: SparkSession, kafka_server: str, topic_prefix: str):
+    def read_stream(self, spark: SparkSession):
+        subscription = f"projects/{self.project_id}/locations/{self.zone}/subscriptions/{self.table_name}"
         self.data = (
-            spark.readStream.format("kafka")
-            .option("kafka.bootstrap.servers", kafka_server)
-            .option("subscribe", topic_prefix + "." + self.table_name)
-            .option("startingOffsets", "earliest")
-            .option("maxOffsetsPerTrigger", Processor.__max_offsets)
+            spark.readStream.format("pubsublite")
+            .option("pubsublite.subscription", subscription)
+            .option(
+                "pubsublite.flowcontrol.maxmessagesperbatch", Processor.__max_messages
+            )
             .load()
         )
         return self
 
     def process_stream(self):
         self.data = (
-            self.data.select(col("value").cast("string").alias("value"))
-            .withColumn("before", get_json_object(col("value"), "$.payload.before"))
-            .withColumn("after", get_json_object(col("value"), "$.payload.after"))
+            self.data.select(col("data").cast("string").alias("data"))
+            .withColumn("before", get_json_object(col("data"), "$.payload.before"))
+            .withColumn("after", get_json_object(col("data"), "$.payload.after"))
             .select(
                 from_json(col("before"), schema_map[self.table_name]).alias("before"),
                 from_json(col("after"), schema_map[self.table_name]).alias("after"),
