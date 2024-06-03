358d0492b6d3ca555c8e47b72ecf743cdf44a4c3
Pinky Gautam <pkgtm1998@gmail.com>
2024-06-04 01:17:44 +0700

    Create temp.py

---
+++ pyspark/temp.py
@@ -0,0 +1,30 @@
+from pyspark.sql import DataFrame
+from pyspark.sql.functions import col, isnotnull, from_json
+from common import temp_clean_map, temp_schema_map
+from processor import Processor
+
+
+class TempTableProcessor(Processor):
+
+    def __insert_records(self, df: DataFrame, batch_id: int):
+        if df.count() == 0:
+            return
+        staging_table = self.staging_dataset + "." + self.table_name
+        self.stage_records(df, staging_table, mode="append")
+
+    def load_stream(self):
+        (
+            self.data.select(
+                from_json(col("before"), temp_schema_map[self.table_name]).alias(
+                    "before"
+                ),
+                from_json(col("after"), temp_schema_map[self.table_name]).alias(
+                    "after"
+                ),
+            )
+            .filter(isnotnull(col("after")))
+            .select("after.*")
+            .selectExpr(*temp_clean_map[self.table_name])
+            .writeStream.foreachBatch(self.__insert_records)
+            .start()
+        )
